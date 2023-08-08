6a1db253bd5262772664ecb0a46e226f5a76de07
unknown <carlritchyalexis99@gmail.com>
2023-08-07 22:57:32 -0400

    first commit coding challenge

---
+++ ENONSE/enonse4.py
@@ -0,0 +1,24 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+... #Enonse 4 - Kalkile epi afiche rezilta adisyon, soustraksyon, miltiplikasyon ak divizyon 2 nonb (antye ak desimal) gras ak operatè matematik ki nan Python yo.
+>>> a= 10
+>>> b = 5.2
+>>> adisyon = a + b
+>>> print (adisyon)
+15.2
+>>> 
+>>> 
+>>> soustraksyon = a - b
+>>> print(soustraksyon)
+4.8
+>>> 
+>>> 
+>>> miltiplikasyon = a * b
+>>> print(miltiplikasyon)
+52.0
+>>> 
+>>> 
+>>> divizyon = a /b
+>>> print(divizyon)
+1.923076923076923
---
+++ ENONSE/enonse5.py
@@ -0,0 +1,15 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #Enonse 5 - Itlize Python, pou w afiche tip 4 diferan tip varyab.
+>>> x = 2
+>>> type(x)
+<class 'int'>
+>>> y= 5.5
+>>> type(y)
+<class 'float'>
+>>> z= True
+>>> type(z)
+<class 'bool'>
+>>> h = "12346"
+>>> type (h)
+<class 'str'>
